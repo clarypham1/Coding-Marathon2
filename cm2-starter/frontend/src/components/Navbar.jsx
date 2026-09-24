3b117a937f1a860f6c89da31169b8370a4f9c72e
Clary <claryphp@metropolia.fi>
2026-09-24 15:22:50 +0300

    implement Navbar.jsx

--- cm2-starter/frontend/src/components/Navbar.jsx
+++ cm2-starter/frontend/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
+import {Link} from 'react-router-dom';
+
 
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
       ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
       : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
-
+  
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -28,6 +30,12 @@ const Navbar = () => {
                 </NavLink>
                 <NavLink to='/add-job' className={linkClass}>
                   Add Job
+                </NavLink>
+                <NavLink to='/login' className={linkClass}>
+                  Login
+                </NavLink>
+                <NavLink to='/signup' className={linkClass}>
+                  Signup
                 </NavLink>
               </div>
             </div>
