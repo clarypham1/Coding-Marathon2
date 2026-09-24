353c37eefe141dcc540b70136bddaa137da913e5
Clary <claryphp@metropolia.fi>
2026-09-24 15:47:08 +0300

    handle pagination with limit

--- cm2-starter/backend/api-server-starter/controllers/jobControllers.js
+++ cm2-starter/backend/api-server-starter/controllers/jobControllers.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 
 //get
 const getAllJobs = async (req, res) => {
-    try {
-        const user_id = req.user_id;
-        const jobs = await Job.fin({ user_id }).sort({ createdAt: -1 });
-        res.status(200).json(jobs);
-    } catch (error) {
-        res.status(500).json({ message: "Failed" });
-    }
+  try {
+    const limit = parseInt(req.query._limit);
+    const jobs = limit
+      ? await Job.find({}).sort({ createdAt: -1 }).limit(limit)
+      : await Job.find({}).sort({ createdAt: -1 });
 
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(500).json({ message: "Failed" });
+  }
 };
 
 //post
