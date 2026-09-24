e8b06cab4c78815212eeb43210ef25d2caf7c3b6
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-24 12:42:04 +0300

    feat: log in

---
+++ cm2-starter/frontend/src/components/Login.jsx
@@ -0,0 +1,60 @@
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = ({ setIsAuthenticated }) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const user = await response.json();
+
+    if (!response.ok) {
+      setError(user.error);
+      return;
+    }
+
+    localStorage.setItem("user", JSON.stringify(user));
+    console.log("success");
+    setIsAuthenticated(true);
+    navigate("/");
+  };
+
+return (
+    <div className="Log - In">
+        <h2> Log in</h2>
+        <form onSubmit={handleFormSubmit}>
+            <label>
+                Email Address:
+            </label>
+
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button>Log in</button>
+
+        </form>
+
+    </div>
+)};
+
+export default Login;
